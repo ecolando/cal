877c1f71620cb12fefc269737c55938cc058bafa
planit <planit@tanjeffreyz.github.io>
2024-09-09 16:40:32 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -115,6 +115,27 @@ assignments = {
       "dueDate": "2024-09-06T23:59:00",
       "link": "https://www.gradescope.com/courses/827938/assignments/4861348/submissions/265076701",
       "submitted": true
+    },
+    {
+      "title": "HW-8",
+      "course": "ME-200 Division-1",
+      "dueDate": "2024-09-13T23:59:00",
+      "link": "https://www.gradescope.com/courses/827938",
+      "submitted": false
+    },
+    {
+      "title": "HW-9",
+      "course": "ME-200 Division-1",
+      "dueDate": "2024-09-13T23:59:00",
+      "link": "https://www.gradescope.com/courses/827938",
+      "submitted": false
+    },
+    {
+      "title": "HW-10",
+      "course": "ME-200 Division-1",
+      "dueDate": "2024-09-13T23:59:00",
+      "link": "https://www.gradescope.com/courses/827938",
+      "submitted": false
     }
   ],
   "wl.202510.MA.30300.319": [
