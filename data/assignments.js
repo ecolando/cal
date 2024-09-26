89d74802d67ef90a50b2a799916994c995e569ce
planit <planit@tanjeffreyz.github.io>
2024-09-26 17:38:31 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -173,6 +173,13 @@ assignments = {
       "dueDate": "2024-11-22T23:59:00",
       "link": "https://www.gradescope.com/courses/831286",
       "submitted": false
+    },
+    {
+      "title": "Week 6 attendance (Sept 24 / 26)",
+      "course": "ME 290",
+      "dueDate": "2024-09-27T23:59:00",
+      "link": "https://www.gradescope.com/courses/831286/assignments/5028402/submissions/271692550",
+      "submitted": true
     }
   ],
   "ME-200 Division-1": [
