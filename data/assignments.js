43c57a7a9a5bb6dc94483f73a137de6748be57fc
planit <planit@tanjeffreyz.github.io>
2024-10-02 12:56:27 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -111,6 +111,13 @@ assignments = {
       "dueDate": "2024-10-02T23:59:00",
       "link": "https://www.gradescope.com/courses/816473",
       "submitted": false
+    },
+    {
+      "title": "Homework 18.A",
+      "course": "ME 270 - 008 LEC",
+      "dueDate": "2024-10-09T23:59:00",
+      "link": "https://www.gradescope.com/courses/816473",
+      "submitted": false
     }
   ],
   "ME 270 - 008 Quiz Submission": [
