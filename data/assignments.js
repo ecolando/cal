f5cf4f08550bfb0952ba048d6de40bce861d9248
planit <planit@tanjeffreyz.github.io>
2024-10-24 18:41:32 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -252,6 +252,13 @@ assignments = {
       "submitted": true
     },
     {
+      "title": "Week 10 attendance (Oct 22 / 24)",
+      "course": "ME 290",
+      "dueDate": "2024-10-25T23:59:00",
+      "link": "https://www.gradescope.com/courses/831286/assignments/5047838/submissions/281587712",
+      "submitted": true
+    },
+    {
       "title": "Netiquette (extra credit)",
       "course": "ME 290",
       "dueDate": "2024-11-22T23:59:00",
