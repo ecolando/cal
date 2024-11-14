9bdae35ebdf53d6e601debf300e66c5410af45b4
planit <planit@tanjeffreyz.github.io>
2024-11-14 14:38:30 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -366,8 +366,8 @@ assignments = {
     {
       "title": "Week 13 attendance (Nov 12 / 14)",
       "course": "ME 290",
-      "dueDate": "2024-11-12T15:00:00",
-      "link": "https://www.gradescope.com/courses/831286",
+      "dueDate": "2024-11-14T10:00:00",
+      "link": "https://www.gradescope.com/courses/831286/assignments/5309494/submissions/new",
       "submitted": false
     },
     {
