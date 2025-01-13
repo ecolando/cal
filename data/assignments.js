5dcd384ed6ecfbf0bf18b80559dc015169d61bf7
planit <planit@tanjeffreyz.github.io>
2025-01-13 14:38:51 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,3 +1,152 @@
 assignments = {
+  "ECE20007-022": [
+    {
+      "title": "Experiment 3 Notebook",
+      "course": "ECE20007-022",
+      "dueDate": "2025-02-13T23:59:00",
+      "link": "https://www.gradescope.com/courses/949439",
+      "submitted": false
+    },
+    {
+      "title": "Experiment 7 Notebook",
+      "course": "ECE20007-022",
+      "dueDate": "2025-03-13T23:59:00",
+      "link": "https://www.gradescope.com/courses/949439",
+      "submitted": false
+    },
+    {
+      "title": "Experiment 6 Notebook",
+      "course": "ECE20007-022",
+      "dueDate": "2025-03-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/949439",
+      "submitted": false
+    },
+    {
+      "title": "Experiment 3 Writeup",
+      "course": "ECE20007-022",
+      "dueDate": "2025-02-13T23:59:00",
+      "link": "https://www.gradescope.com/courses/949439",
+      "submitted": false
+    },
+    {
+      "title": "Experiment 10 Notebook",
+      "course": "ECE20007-022",
+      "dueDate": "2025-04-17T23:59:00",
+      "link": "https://www.gradescope.com/courses/949439",
+      "submitted": false
+    },
+    {
+      "title": "Experiment 2 Notebook",
+      "course": "ECE20007-022",
+      "dueDate": "2025-02-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/949439",
+      "submitted": false
+    },
+    {
+      "title": "Experiment 1 Writeup",
+      "course": "ECE20007-022",
+      "dueDate": "2025-01-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/949439",
+      "submitted": false
+    },
+    {
+      "title": "Experiment 6 Writeup",
+      "course": "ECE20007-022",
+      "dueDate": "2025-03-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/949439",
+      "submitted": false
+    },
+    {
+      "title": "Experiment 7 Writeup",
+      "course": "ECE20007-022",
+      "dueDate": "2025-03-13T23:59:00",
+      "link": "https://www.gradescope.com/courses/949439",
+      "submitted": false
+    },
+    {
+      "title": "Experiment 8 Notebook",
+      "course": "ECE20007-022",
+      "dueDate": "2025-03-27T23:59:00",
+      "link": "https://www.gradescope.com/courses/949439",
+      "submitted": false
+    },
+    {
+      "title": "Experiment 5 Notebook",
+      "course": "ECE20007-022",
+      "dueDate": "2025-02-27T23:59:00",
+      "link": "https://www.gradescope.com/courses/949439",
+      "submitted": false
+    },
+    {
+      "title": "Mini Project Notebook",
+      "course": "ECE20007-022",
+      "dueDate": "2025-04-10T23:59:00",
+      "link": "https://www.gradescope.com/courses/949439",
+      "submitted": false
+    },
+    {
+      "title": "Experiment 1 Notebook",
+      "course": "ECE20007-022",
+      "dueDate": "2025-01-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/949439",
+      "submitted": false
+    },
+    {
+      "title": "Experiment 2 Writeup",
+      "course": "ECE20007-022",
+      "dueDate": "2025-02-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/949439",
+      "submitted": false
+    },
+    {
+      "title": "Wheatstone Bridge Bonus Project Report",
+      "course": "ECE20007-022",
+      "dueDate": "2025-02-27T23:59:00",
+      "link": "https://www.gradescope.com/courses/949439",
+      "submitted": false
+    },
+    {
+      "title": "Experiment 5 Writeup",
+      "course": "ECE20007-022",
+      "dueDate": "2025-02-27T23:59:00",
+      "link": "https://www.gradescope.com/courses/949439",
+      "submitted": false
+    },
+    {
+      "title": "Mini Project Report",
+      "course": "ECE20007-022",
+      "dueDate": "2025-04-10T23:59:00",
+      "link": "https://www.gradescope.com/courses/949439",
+      "submitted": false
+    },
+    {
+      "title": "Final Project Report",
+      "course": "ECE20007-022",
+      "dueDate": "2025-05-01T23:59:00",
+      "link": "https://www.gradescope.com/courses/949439",
+      "submitted": false
+    },
+    {
+      "title": "Experiment 10 Writeup",
+      "course": "ECE20007-022",
+      "dueDate": "2025-04-17T23:59:00",
+      "link": "https://www.gradescope.com/courses/949439",
+      "submitted": false
+    },
+    {
+      "title": "Final Project Notebook",
+      "course": "ECE20007-022",
+      "dueDate": "2025-05-01T23:59:00",
+      "link": "https://www.gradescope.com/courses/949439",
+      "submitted": false
+    },
+    {
+      "title": "Experiment 8 Writeup",
+      "course": "ECE20007-022",
+      "dueDate": "2025-03-27T23:59:00",
+      "link": "https://www.gradescope.com/courses/949439",
+      "submitted": false
+    }
+  ],
   "ME 274 - Krousgrill - 1:30 - Spring 2025": []
 };
