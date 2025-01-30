9690c78b67f6adbe7d0952ccf4ebaa08fa1708dc
planit <planit@tanjeffreyz.github.io>
2025-01-30 15:34:18 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -250,6 +250,13 @@ assignments = {
       "submitted": false
     },
     {
+      "title": "Deliverable 7",
+      "course": "wl.202520.ME.26300.220",
+      "dueDate": "2025-02-06T10:30:00",
+      "link": "https://www.gradescope.com/courses/952581",
+      "submitted": false
+    },
+    {
       "title": "Paper Draft #1 Introduction and Project Plan",
       "course": "wl.202520.ME.26300.220",
       "dueDate": "2025-01-30T23:59:00",
