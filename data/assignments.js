3e1db055c818b45bc83ee5f3a5386cbc8a3709f4
planit <planit@tanjeffreyz.github.io>
2025-02-26 10:39:41 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -277,6 +277,13 @@ assignments = {
       "submitted": false
     },
     {
+      "title": "Homework 18 - (H.4.E, H.4.F)",
+      "course": "ME 274 - Krousgrill - 1:30 - Spring 2025",
+      "dueDate": "2025-02-28T23:59:00",
+      "link": "https://www.gradescope.com/courses/948731",
+      "submitted": false
+    },
+    {
       "title": "Exam 1",
       "course": "ME 274 - Krousgrill - 1:30 - Spring 2025",
       "dueDate": "2025-02-12T22:00:00",
