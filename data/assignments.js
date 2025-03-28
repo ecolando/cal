fbaa44cdab0558383a59fc4f52bd37c2cc4f8204
planit <planit@tanjeffreyz.github.io>
2025-03-28 14:39:03 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -510,6 +510,13 @@ assignments = {
       "submitted": true
     },
     {
+      "title": "Group Evaluation #2",
+      "course": "wl.202520.ME.26300.220",
+      "dueDate": "2025-04-01T23:59:00",
+      "link": "https://www.gradescope.com/courses/952581",
+      "submitted": false
+    },
+    {
       "title": "Presentation - FINAL",
       "course": "wl.202520.ME.26300.220",
       "dueDate": "2025-03-27T23:59:00",
